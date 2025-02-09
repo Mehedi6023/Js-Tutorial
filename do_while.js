ed97307afa69f40e12a1b2a5c84fcedc319a1792
Mehedi6023 <quazimehedihasanovi@gmail.com>
2025-02-09 16:45:45 +0600

    added do while

---
+++ do_while.js
@@ -0,0 +1,14 @@
+//while loop
+const arr = [2, 4, 5, 6, 8, 8, 4, 7, 0, 1];
+let i = 1;
+while (i <= 9) {
+  console.log("The number is : " + arr[i]);
+  i++;
+}
+//do while loop
+const arr2 = ["ovi", "raj", "qe", "ri", "ur", "ovi", "raj", "qe", "ri", "ur"];
+let j = 1;
+do {
+  console.log("The name is : " + arr2[j]);
+  j++;
+} while (j <= 9);
